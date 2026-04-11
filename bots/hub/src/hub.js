ade6e4eb93f2055ce598445f718442c74c0305ec
AlexLee00 <leejearyong@gmail.com>
2026-04-11 18:10:37 +0900

    feat(elixir): scaffold investment port supervisor

--- bots/hub/src/hub.js
+++ bots/hub/src/hub.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const path = require('node:path');
+const env = require('../../../packages/core/lib/env');
 
-const runtimePath = path.join(__dirname, '../../../dist/ts-runtime/bots/hub/src/hub.js');
+const tsPath = path.join(env.PROJECT_ROOT, 'bots', 'hub', 'src', 'hub.ts');
 
 try {
-  require(runtimePath);
+  require(tsPath);
 } catch (error) {
-  if (error && (error.code === 'MODULE_NOT_FOUND' || error.code === 'ERR_REQUIRE_ESM')) {
+  if (error && (error.code === 'MODULE_NOT_FOUND' || error.code === 'ERR_REQUIRE_ESM' || error.code === 'ERR_UNSUPPORTED_TYPESCRIPT_SYNTAX')) {
     require('./hub.legacy.js');
   } else {
     throw error;
