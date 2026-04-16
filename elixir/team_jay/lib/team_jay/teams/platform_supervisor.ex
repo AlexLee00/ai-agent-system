63b408ea91d307dac4b1be371ca481fd905b7bab
AlexLee00 <leejearyong@gmail.com>
2026-04-16 14:53:26 +0900

    Promote hub resource API to Elixir ownership

--- elixir/team_jay/lib/team_jay/teams/platform_supervisor.ex
+++ elixir/team_jay/lib/team_jay/teams/platform_supervisor.ex
@@ -5,7 +5,8 @@ defmodule TeamJay.Teams.PlatformSupervisor do
     %{
       name: :hub_resource_api,
       script: "bots/hub/src/hub.js",
-      schedule: nil
+      schedule: if(Mix.env() == :test, do: nil, else: :once),
+      health_url: "http://127.0.0.1:7788/hub/health"
     }
   ]
 
@@ -21,7 +22,8 @@ defmodule TeamJay.Teams.PlatformSupervisor do
          name: agent.name,
          team: :platform,
          script: agent.script,
-         schedule: agent.schedule}
+         schedule: agent.schedule,
+         health_url: Map.get(agent, :health_url)}
       end)
 
     Supervisor.init(children, strategy: :one_for_one, max_restarts: 3, max_seconds: 60)
